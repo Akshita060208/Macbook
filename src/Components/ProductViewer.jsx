1a3591af48f0cc79c8f4bf80b973a0e27300ac3e
Akshita060208 <joshiakshita2008@gmail.com>
2026-06-27 17:27:56 +0530

    added 3d model

--- src/Components/ProductViewer.jsx
+++ src/Components/ProductViewer.jsx
@@ -4,18 +4,9 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import '../index.css';
 import useMacbookStore from '../store';
+import MacbookModel14 from './models/Macbook-14';
 
-const RotatingBox = ({ color, scale }) => {
-  const boxColor = color === 'silver' ? '#d8d8d8' : '#101010';
-  const boxScale = scale === 0.06 ? 1.1 : 1.4;
 
-  return (
-    <mesh scale={boxScale}>
-      <boxGeometry args={[1.6, 1.2, 1.2]} />
-      <meshStandardMaterial color={boxColor} metalness={0.8} roughness={0.2} />
-    </mesh>
-  );
-};
 
 const ProductViewer = () => {
   const { color, scale, setColor, setScale } = useMacbookStore();
@@ -47,25 +38,29 @@ const ProductViewer = () => {
               <p>14"</p>
             </div>
             <div
-              onClick={() => setScale(0.8)}
-              className={clsx('bg-neutral-300', scale === 0.8 ? 'bg-white text-black' : 'bg-transparent text-white')}
+              onClick={() => setScale(0.08)}
+              className={clsx('bg-neutral-300', scale === 0.08 ? 'bg-white text-black' : 'bg-transparent text-white')}
             >
               <p>16"</p>
             </div>
           </div>
         </div>
       </div>
+    <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}>
 
-      <Canvas camera={{ position: [0, 0, 5], fov: 40 }} style={{ width: '100%', height: '420px', marginTop: '2rem' }}>
-        <ambientLight intensity={1.2} />
-        <directionalLight position={[3, 3, 5]} intensity={2.5} />
-        <directionalLight position={[-3, 1, -2]} intensity={0.7} />
-        <pointLight position={[0, 2, 3]} intensity={1.2} />
+       <ambientLight intensity={1} />
+  <directionalLight position={[5, 5, 5]} intensity={2} />
 
-        <RotatingBox color={color} scale={scale} />
-        
-        <OrbitControls enableZoom={false} autoRotate autoRotateSpeed={1.4} />
-      </Canvas>
+  <MacbookModel14
+    scale={0.06}
+    position={[0, 0, 0]}
+  />
+
+          
+
+      <OrbitControls enableZoom={false} />
+    </Canvas>
+      
     </section>
   );
 };
